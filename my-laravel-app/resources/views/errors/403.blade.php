d8dc9d7947efcfb1b3e99bf64b6a7aced418ae6b
Charity Samuel <scharity463@gmail.com>
2024-08-19 17:45:54 +0300

    my first commit

---
+++ my-laravel-app/resources/views/errors/403.blade.php
@@ -0,0 +1,5 @@
+<x-app-layout>
+    <div class="flex items-center justify-center min-h-screen bg-gray-100">
+        <h1 class="text-4xl font-semibold text-red-600">Access Denied</h1>
+    </div>
+</x-app-layout>
